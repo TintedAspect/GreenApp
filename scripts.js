d5786dfeff8edad099645537f3a60e5d17b87176
saturner469510 <55389310+saturner469510@users.noreply.github.com>
2019-11-26 09:53:06 +0000

    Contains all the vue and js for fetching the articles.

---
+++ scripts.js
@@ -0,0 +1,33 @@
+var options = {year: 'numeric', month: 'numeric', day: 'numeric'};
+var date = new Date();
+var m = date.getMonth();
+date.setMonth(m-1);
+
+date = date.toLocaleDateString("ko-KR", options).replace(/. /g, "-");
+var len = date.length;
+date = date.slice(0, (len-1));
+console.log(date);
+
+var news = new Vue({
+  el: '#outer',
+  data: {
+    articles: []
+  },
+  created() {
+    var vm = this
+        axios.get('https://newsapi.org/v2/everything?' +
+          'q=sustainable+living|' +
+          'renewable|' +
+          'climate|' +
+          'environment|' +
+          'zero+waste&' +
+          'from=' + date + '&' +
+          'sortBy=popularity&' +
+          'apiKey=d292b50c7ef04e53846162db003c65d4')
+          .then(function(response) {
+            vm.articles = response.data.articles
+          })
+
+  }
+
+})
